b4f8f46d99cbb9839fd423f75562ef3bce1f3fb1
Christiaan Maks <christilut@gmail.com>
2017-11-01 19:25:05 +0100

    fix and merge graphql

--- config/graphql/merge.ts
+++ config/graphql/merge.ts
@@ -1,24 +1,35 @@
+import env from 'config/env'
 import * as path from 'path'
 import * as fs from 'fs'
 import { fileLoader, mergeTypes, mergeResolvers, GraphQlSchema } from 'merge-graphql-schemas'
 import * as GraphQl from 'graphql-tools'
 
-let schema: GraphQlSchema
-let adminSchema: GraphQlSchema
 
 const modelDir = path.join(__dirname, '../../server/models')
 
-// Note that in this file we use .js because the compiled code that is executed isnt Typescript but Javascript
+// Note that in this file we use .js because the compiled code that is executed isnt Typescript but Javascript. However, in Development we use ts-node which executes .ts files directly, so we load those.
+let extension = '.js'
+if (env.NODE_ENV === env.Environments.Development) {
+  extension = '.ts'
+}
 
 // Default schema, for graphql endpoint available to all authenticated users
-{
+function merge(isAdmin: boolean): GraphQlSchema {
   const schemas: string[] = []
   const resolvers: string[] = []
 
   fs.readdirSync(modelDir).forEach(dir => {
-    if (dir.lastIndexOf('.js') === dir.length - '.js'.length) return
+    if (dir.lastIndexOf(extension) === dir.length - extension.length) return
 
-    const dirPath = path.join(modelDir, dir)
+    let dirPath = path.join(modelDir, dir)
+
+    if (isAdmin) {
+      // Load shared schema
+      schemas.push(fs.readFileSync(path.join(dirPath, 'shared.gql'), 'utf8'))
+
+      // Then change to admin subfolder path
+      dirPath = path.join(dirPath, 'admin')
+    }
 
     // load schemas
     fs.readdirSync(dirPath).forEach(file => {
@@ -29,44 +40,8 @@ const modelDir = path.join(__dirname, '../../server/models')
 
     // load resolvers
     fs.readdirSync(dirPath).forEach(file => {
-      if (file === 'resolvers.js') {
+      if (file === 'resolvers' + extension) {
         resolvers.push(require(path.join(dirPath, file)).default)
-      }
-    })
-  })
-
-  const mergedSchemas = mergeTypes(schemas)
-  const mergedResolvers = resolvers.length > 1 ? mergeResolvers(resolvers) : resolvers[0]
-
-  schema = GraphQl.makeExecutableSchema({ typeDefs: mergedSchemas, resolvers: mergedResolvers })
-}
-
-// Admin schema, for graphql endpoint only available to admins
-{ // TODO combine these 2
-  const schemas: string[] = []
-  const resolvers: string[] = []
-
-  fs.readdirSync(modelDir).forEach(dir => {
-    if (dir.lastIndexOf('.js') === dir.length - '.js'.length) return
-
-    const dirPath = path.join(modelDir, dir)
-    const dirAdminPath = path.join(dirPath, 'admin')
-
-    if (!fs.existsSync(dirAdminPath)) return
-
-    // load admin schemas
-    schemas.push(fs.readFileSync(path.join(dirPath, 'shared.gql'), 'utf8'))
-
-    fs.readdirSync(dirAdminPath).forEach(file => {
-      if (file.includes('.gql')) {
-        schemas.push(fs.readFileSync(path.join(dirAdminPath, file), 'utf8'))
-      }
-    })
-
-    // load admin resolvers
-    fs.readdirSync(dirAdminPath).forEach(file => {
-      if (file === 'resolvers.js') {
-        resolvers.push(require(path.join(dirAdminPath, file)).default)
       }
     })
   })
@@ -75,9 +50,14 @@ const modelDir = path.join(__dirname, '../../server/models')
     const mergedSchemas = mergeTypes(schemas)
     const mergedResolvers = resolvers.length > 1 ? mergeResolvers(resolvers) : resolvers[0]
 
-    adminSchema = GraphQl.makeExecutableSchema({ typeDefs: mergedSchemas, resolvers: mergedResolvers })
+    if (resolvers.length === 0) throw new Error('No resolvers found')
+
+    return GraphQl.makeExecutableSchema({ typeDefs: mergedSchemas, resolvers: mergedResolvers })
   }
 }
+
+const schema: GraphQlSchema = merge(false)
+const adminSchema: GraphQlSchema = merge(true)
 
 export {
   schema,
