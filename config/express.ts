d45a4b5d77e3dd30703a7957914f95db2805b8ce
Christiaan Maks <christilut@gmail.com>
2017-11-01 20:32:46 +0100

    get free port instead of random

--- config/express.ts
+++ config/express.ts
@@ -14,157 +14,163 @@ import { APIError } from 'server/helpers/APIError'
 import { graphiqlExpress } from 'apollo-server-express'
 import { checkAuthentication, checkAdminRole } from 'server/controllers/auth.controller'
 import { Express } from 'express-serve-static-core'
+import * as getPort from 'get-port'
 
-const app: Express = express()
-
-const router = expressPromiseRouter()
-
-if (env.NODE_ENV === env.Environments.Development) {
-  app.use(Morgan('dev')) // HTTP request logging
-}
-
-// Parse body params and set them to req.body
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(compress()) // Enable compression
-app.enable('trust proxy') // Needed if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
-app.use(helmet()) // Secure apps by setting various HTTP headers
-
-// Load Forest Admin
-require('config/forestadmin')(app)
-
-// Enable CORS - Cross Origin Resource Sharing
-if (env.NODE_ENV === env.Environments.Production) {
-  app.use(cors())
-  // app.use(cors({
-  //   origin: [], // TODO add origins for production
-  //   optionsSuccessStatus: 200
-  // }))
-
-  // Enable Forest Admin (must be done before express error handlers)
-} else {
-  app.use(cors())
-}
-
-// Public routes for login and registration
 import publicRoutes from 'server/public_routes'
-app.use('/', publicRoutes)
-
-// Private GraphQL routes, require authentication
 import { graphQlRoute, graphQlAdminRoute } from 'config/graphql'
 
-if (env.NODE_ENV === env.Environments.Development) {
-  app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
-  app.use('/admin-graphiql', graphiqlExpress({ endpointURL: '/admin-graphql' }))
-}
+let app: Express
 
-router.use('/graphql', bodyParser.json(), checkAuthentication, graphQlRoute)
-router.use('/admin-graphql', bodyParser.json(), checkAuthentication, checkAdminRole, graphQlAdminRoute)
-app.use('/', router)
+(async () => {
+  app = express()
 
-// Enable detailed API logging
-if (env.NODE_ENV !== env.Environments.Test) {
-  expressWinston.requestWhitelist.push('body')
-  expressWinston.responseWhitelist.push('body')
-  app.use(expressWinston.logger({
-    winstonInstance: logger,
-    meta: true, // optional: log meta data about request (defaults to true)
-    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-    colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
-  }))
-}
+  const router = expressPromiseRouter()
 
-// If error is not an instanceOf APIError, convert it.
-app.use((err, req, res, next) => {
-  if (!(err instanceof APIError)) {
-    const apiError = new APIError(err.message, err.status, err.isPublic)
-
-    return next(apiError)
-  }
-  return next(err)
-})
-
-// Catch 404 and forward to error handler
-app.use((req, res, next) => {
-  if (req.url === '/favicon.ico') {
-    return res.sendStatus(httpStatus.NOT_FOUND)
+  if (env.NODE_ENV === env.Environments.Development) {
+    app.use(Morgan('dev')) // HTTP request logging
   }
 
-  const err = new APIError('API not found', httpStatus.NOT_FOUND)
-  return next(err)
-})
+  // Parse body params and set them to req.body
+  app.use(bodyParser.json({ limit: '50mb' }))
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-// Error handler, send stacktrace only during development
-app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  app.use(compress()) // Enable compression
+  app.enable('trust proxy') // Needed if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+  app.use(helmet()) // Secure apps by setting various HTTP headers
 
-  // Filter errors that are not really errors
+  // Load Forest Admin
+  require('config/forestadmin')(app)
+
+  // Enable CORS - Cross Origin Resource Sharing
+  if (env.NODE_ENV === env.Environments.Production) {
+    app.use(cors())
+    // app.use(cors({
+    //   origin: [], // TODO add origins for production
+    //   optionsSuccessStatus: 200
+    // }))
+
+    // Enable Forest Admin (must be done before express error handlers)
+  } else {
+    app.use(cors())
+  }
+
+  // Public routes for login and registration
+  app.use('/', publicRoutes)
+
+  // Private GraphQL routes, require authentication
+
+  if (env.NODE_ENV === env.Environments.Development) {
+    app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+    app.use('/admin-graphiql', graphiqlExpress({ endpointURL: '/admin-graphql' }))
+  }
+
+  router.use('/graphql', bodyParser.json(), checkAuthentication, graphQlRoute)
+  router.use('/admin-graphql', bodyParser.json(), checkAuthentication, checkAdminRole, graphQlAdminRoute)
+  app.use('/', router)
+
+  // Enable detailed API logging
   if (env.NODE_ENV !== env.Environments.Test) {
-    if (err instanceof APIError) {
-      logger.warn('Handled API error', {
-        err,
-        request: {
+    expressWinston.requestWhitelist.push('body')
+    expressWinston.responseWhitelist.push('body')
+    app.use(expressWinston.logger({
+      winstonInstance: logger,
+      meta: true, // optional: log meta data about request (defaults to true)
+      msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+      colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+    }))
+  }
+
+  // If error is not an instanceOf APIError, convert it.
+  app.use((err, req, res, next) => {
+    if (!(err instanceof APIError)) {
+      const apiError = new APIError(err.message, err.status, err.isPublic)
+
+      return next(apiError)
+    }
+    return next(err)
+  })
+
+  // Catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    if (req.url === '/favicon.ico') {
+      return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+
+    const err = new APIError('API not found', httpStatus.NOT_FOUND)
+    return next(err)
+  })
+
+  // Error handler, send stacktrace only during development
+  app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+
+    // Filter errors that are not really errors
+    if (env.NODE_ENV !== env.Environments.Test) {
+      if (err instanceof APIError) {
+        logger.warn('Handled API error', {
+          err,
+          request: {
+            url: req.url,
+            params: req.params,
+            query: req.query,
+            method: req.method,
+            body: req.body
+          }
+        })
+      } else {
+        const headersWithoutAuth = req.headers
+        delete headersWithoutAuth.authorization
+
+        const request = {
           url: req.url,
           params: req.params,
           query: req.query,
           method: req.method,
+          headers: headersWithoutAuth,
           body: req.body
         }
+
+        logger.error('Unhandled exception occurred', {
+          err,
+          stack: err.stack,
+          request
+        })
+
+        if (env.NODE_ENV === env.Environments.Production) {
+          Raven.captureException(err, {
+            request
+          } as Raven.CaptureOptions)
+        }
+      }
+    }
+
+    if (env.NODE_ENV === env.Environments.Test) {
+      if (err.status === httpStatus.INTERNAL_SERVER_ERROR) { // TODO needed?
+        console.log('Internal Server Error:', err.message)
+      }
+    }
+
+    // Determines what to return to user. Add error stack in development. Only add error message if it is set as public.
+    if (env.NODE_ENV === env.Environments.Development) {
+      res.status(err.status).json({
+        message: err.isPublic ? err.message : httpStatus[err.status],
+        stack: err.stack
       })
     } else {
-      const headersWithoutAuth = req.headers
-      delete headersWithoutAuth.authorization
-
-      const request = {
-        url: req.url,
-        params: req.params,
-        query: req.query,
-        method: req.method,
-        headers: headersWithoutAuth,
-        body: req.body
-      }
-
-      logger.error('Unhandled exception occurred', {
-        err,
-        stack: err.stack,
-        request
+      res.status(err.status).json({
+        message: err.isPublic ? err.message : httpStatus[err.status]
       })
-
-      if (env.NODE_ENV === env.Environments.Production) {
-        Raven.captureException(err, {
-          request
-        } as Raven.CaptureOptions)
-      }
     }
+  })
+
+  const port = env.NODE_ENV === env.Environments.Test ? await getPort() : env.PORT
+
+  app.listen(port, () => {
+    logger.info(`Server started on port ${port} (${env.NODE_ENV})`)
+  })
+
+  if (env.NODE_ENV !== env.Environments.Test) {
+    console.log('Express: Loaded')
   }
-
-  if (env.NODE_ENV === env.Environments.Test) {
-    if (err.status === httpStatus.INTERNAL_SERVER_ERROR) { // TODO needed?
-      console.log('Internal Server Error:', err.message)
-    }
-  }
-
-  // Determines what to return to user. Add error stack in development. Only add error message if it is set as public.
-  if (env.NODE_ENV === env.Environments.Development) {
-    res.status(err.status).json({
-      message: err.isPublic ? err.message : httpStatus[err.status],
-      stack: err.stack
-    })
-  } else {
-    res.status(err.status).json({
-      message: err.isPublic ? err.message : httpStatus[err.status]
-    })
-  }
-})
-
-const port = env.NODE_ENV === env.Environments.Test ? Math.floor((Math.random() * (65535 - 1000) + 1000)) : env.PORT // Start tests on a random port because each test file runs in parallel and will start its own express server
-
-app.listen(port, () => {
-  logger.info(`Server started on port ${port} (${env.NODE_ENV})`)
-})
+})()
 
 export default app
-
-if (env.NODE_ENV !== env.Environments.Test) {
-  console.log('Express: Loaded')
-}
