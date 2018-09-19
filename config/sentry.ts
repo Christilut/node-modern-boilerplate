cb58cd8cf78f6992a203e83806a817b955af35bc
Christiaan Maks <christilut@gmail.com>
2018-09-19 16:48:57 +0200

    add sentry filters

--- config/sentry.ts
+++ config/sentry.ts
@@ -1,12 +1,26 @@
 import env from 'config/env'
 import * as Raven from 'raven'
+import * as httpStatus from 'http-status'
+
+function sentryFilter(data) {
+  const statusCode = data.res.statusCode
+
+  // Filter auth errors
+  if (statusCode === httpStatus.UNAUTHORIZED) return false
+
+  // Filter users trying things they shouldnt
+  if (statusCode === httpStatus.FORBIDDEN) return false
+
+  return true
+}
 
 if (env.SENTRY_URL) {
   Raven.config(env.SENTRY_URL, {
-    environment: env.NODE_ENV
+    environment: env.NODE_ENV,
+    shouldSendCallback: sentryFilter
   }).install()
 
   console.log('Sentry: Loaded')
 } else {
-  console.log('Sentry: Missing Sentry credentials, not loading')
+  console.log('Sentry: tMissing Sentry credentials, not loading')
 }
